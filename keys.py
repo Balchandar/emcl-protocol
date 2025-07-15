babcdc85e82946a8d4deb17c20a26233cb20b45f
Bala <55577715+Balchandar@users.noreply.github.com>
2025-07-15 20:28:58 +0530

    Create keys.py
    Init

---
+++ keys.py
@@ -0,0 +1,3 @@
+# WARNING: DO NOT use hardcoded keys in production
+aes_key = b'0123456789abcdef0123456789abcdef'  # 32 bytes AES key
+hmac_key = b'fedcba9876543210fedcba9876543210'  # 32 bytes HMAC key
